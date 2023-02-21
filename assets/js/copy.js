9f8919e6f176bc0c2e8b673413feba5f5c29bd47
Ivy Basseches <imb2139@columbia.edu>
2023-02-21 15:27:11 -0500

    Code copy to clipboard icon

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,26 +1,46 @@
-var codeBlocks = document.querySelectorAll('pre.highlight');
+// select all markdown code blocks
+let codeBlocks = document.querySelectorAll('pre.highlight');
 
 codeBlocks.forEach(function(codeBlock) {
-    var copyButton = document.createElement('button');
+    let copyButton = document.createElement('button');
     copyButton.className = 'copy';
     copyButton.type = 'button';
-    copyButton.ariaLabel = 'Copy code to clipboard';
-    copyButton.innerText = 'Copy';
+    copyButton.ariaLabel = 'Copy to clipboard';
 
+    let clipboard = document.createElement('img');
+    clipboard.src = "/latex-savvy/assets/static/copy.png";
+    clipboard.width = 30;
+
+    copyButton.append(clipboard);
     codeBlock.append(copyButton);
 
     copyButton.addEventListener('click', function () {
-        var code = codeBlock.querySelector('code').innerText.trim();
+
+        // copy text
+        let code = codeBlock.querySelector('code').innerText.trim();
         window.navigator.clipboard.writeText(code);
 
-        copyButton.innerText = 'Copied!';
-        var fourSeconds = 4000;
+        // add checkmark to indicate that the text has been copied
+        copyButton.innerText = '';
+        let check = document.createElement('img');
+        check.src = "/latex-savvy/assets/static/checkmark.png";
+        check.width = 30;
+        copyButton.append(check);
 
+        let fourSeconds = 4000;
+
+        // after 4 seconds, show the copy to clipboard icon again
         setTimeout(function () {
-        copyButton.innerText = 'Copy';
+            let clipboard = document.createElement('img');
+            clipboard.src = "/latex-savvy/assets/static/copy.png";
+            clipboard.width = 30;
+            copyButton.innerText = '';
+            copyButton.append(clipboard);
         }, fourSeconds);
+
     });
 
+    // when the user hovers over the code block, show the button to copy
     codeBlock.addEventListener('mouseover', function () {
         copyButton.focus({preventScroll:true});
     });
